0899ca88280c030b2621b7da4270f352f91d9f71
eanp <awifamz@gmail.com>
2023-02-24 16:17:58 +0700

    cyclic url and storage

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -36,7 +36,8 @@ const UsersController = {
         }
 
         try{
-            let url = `http://${process.env.BASE_URL}:${process.env.PORT}/auth/otp/${id}/${otp}`
+            let url = `https://zealous-erin-cod.cyclic.app//auth/otp/${id}/${otp}`
+            // let url = `http://${process.env.BASE_URL}:${process.env.PORT}/auth/otp/${id}/${otp}`
             let sendEmail =  email(req.body.email,otp,url,req.body.name)
             if(sendEmail == 'email not send'){
                 return res.status(404).json({status:404,message:`register gagal, email tidak terkirim`})                
